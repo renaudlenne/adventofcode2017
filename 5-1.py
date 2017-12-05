959dab22db4bf91a7dcb9407b9361e317fb0a8a9
Renaud Lenne <renaud@26lights.com>
2017-12-05 09:57:51 +0100

    5th day puzzles

---
+++ 5-1.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python
+import sys
+from collections import Counter
+
+if __name__ == "__main__":
+    nb_args = len(sys.argv)
+    if nb_args != 2:
+        print 'Syntax : %s <input>' % sys.argv[0]
+        exit(1)
+
+    filepath = sys.argv[1]
+    nb_valid = 0
+    with open(filepath) as fp:
+        content = fp.readlines()
+        content = [int(x.strip()) for x in content]
+        nb_steps = 0
+        i = 0
+        while 0 <= i < len(content):
+            nb_steps += 1
+            prev_i = i
+            i += content[prev_i]
+            content[prev_i] += 1
+        print nb_steps
